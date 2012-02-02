718835baf6abe7f86e6356bbe21b84981907d919
Mike G <gorbachevma@gmail.com>
2012-02-02 01:18:49 -0500

    added login.xml, LoginActivity.java
    fixed some bugs. Still won't run

---
+++ fbConnect/src/com/wkcof/fbconnect/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.wkcof.fbconnect;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+
+	}
+	
+
+}
